e547532e16e3e793aa684ee75f18567342a9e6c0
jaelc <jflaquer07@gmail.com>
2021-03-07 03:21:40 -0500

    Added the Two Sum problem

---
+++ TwoSum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    #Example Input: 
+    #nums = [2,7,11,15]
+    #target = 9
+
+    #Example Output:
+    #[0,1]
+
+    """
+    Given an array of integers (nums) and an integer (target), 
+    return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, 
+    and you may not use the same element twice.
+    You can return the answer in any order.
+
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]     
+    """
+
+    def twoSum(self, nums, target):
